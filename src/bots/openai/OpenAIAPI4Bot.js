8567d14a69b321d4895bfa2255edb7de243c74f8
Yihuki <wangbovbvb@gmail.com>
2024-04-28 08:09:54 +0800

    切换openai和gemini模型到gpt-4-turbo和gemini-1.5

--- src/bots/openai/OpenAIAPI4Bot.js
+++ src/bots/openai/OpenAIAPI4Bot.js
@@ -4,7 +4,7 @@ export default class OpenAIAPI4Bot extends OpenAIAPIBot {
   static _className = "OpenAIAPI4Bot"; // Class name of the bot
   static _logoFilename = "openai-4-logo.png"; // Place it in public/bots/
   static _isDarkLogo = true; // The main color of logo is dark
-  static _model = "gpt-4-turbo-preview";
+  static _model = "gpt-4-turbo";
 
   constructor() {
     super();
